2bc90d4063322cafcafc4ad59b9832f5385e7fd3
Jo Somers <sayhello@josomers.be>
2013-10-24 16:41:02 +0200

    back to r13 of the support library!

--- setup/maven.sh
+++ setup/maven.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mvn install:install-file \
-    -Dfile=libs/support-v4-r18.jar \
+    -Dfile=libs/support-v4-r13.jar \
     -DgroupId=com.google.android \
     -DartifactId=support-v4 \
-    -Dversion=r18 \
+    -Dversion=r13 \
     -Dpackaging=jar
